0db183da341ae705fa83cd4305a80bc284aed199
matsumoto mika <mk.mtmt48@gmail.com>
2025-08-13 21:39:25 +0900

    global.sassの読み込みパス修正

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter } from 'react-router'
 import RootLayout from '../layout/RootLayout'
 import AppRoutes from './AppRoutes'
-import '../assets/stylesheets/global.sass'
+import '@/assets/stylesheets/global.sass'
 
 const App = () => {
   return (
