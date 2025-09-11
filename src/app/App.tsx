98331d557703a24e45a373e4f695e267ce1631ed
hiromigo <hrm1219.tennis@gmail.com>
2025-09-11 20:59:20 +0900

    radixをimport

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter } from 'react-router'
 import RootLayout from '@/layout/RootLayout'
 import AppRoutes from './AppRoutes'
+import "@radix-ui/themes/styles.css";
 import '@/assets/stylesheets/global.sass'
 
 const App = () => {
--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -2,16 +2,24 @@ import React from 'react';
 import { Header } from './Header';
 import { Footer } from './Footer';
 import { Main } from './Main';
+import { Theme } from "@radix-ui/themes";
+import { Flex, Text, Button } from "@radix-ui/themes";
 type RootLayoutProps = {
   children: React.ReactNode;
 };
 const RootLayout = ({ children }: RootLayoutProps) => {
     return (
-      <div>
-        <Header />
-        <Main>{children}</Main>
-        <Footer />
-      </div>
+      <Theme>
+        
+        <Flex direction="column" gap="2">
+          <Header />
+          <Text>Hello from Radix Themes :)</Text>
+          <Button color="cyan">Let's go</Button>
+          <Button color="crimson" variant="soft">Edit profile</Button>
+          <Main>{children}</Main>
+          <Footer />
+        </Flex>
+      </Theme>
     )
   }
 export default RootLayout
